dbcc6511d0eb7b27330544b164d25a96d5e16643
khkiiim01 <khkiiim01@gmail.com>
2025-02-07 07:35:27 +0900

    LoginMain 페이지 완성, 폰트 추가 요망

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-export default function About() {
+'use client'
+
+import { Button } from "./components/button/Button";
+
+export default function LoginMain() {
   return (
     <main>
       <h1>페이지</h1>
       <p>페이지입니다.</p>
+      <Button label={'활성화된 버튼'} />
+      <hr/>
+    
+      <Button label={'비활성화 버튼'} isActive={false}/>
     </main>
   );
 }
