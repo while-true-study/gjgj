74584660351c182d4c99b1f98852ed891aa9ecdb
Maeng <aodaod128@naver.com>
2025-02-07 23:50:16 +0900

    add inputpage

--- app/components/input/Input.tsx
+++ app/components/input/Input.tsx
@@ -7,15 +7,16 @@ interface InputProps {
   type: string;
   name: string;
   label: string;
-  className?: string;
+  id: string;
+  classname?: string;
   placeholder?: string;
 }
 
-const Input = ({ type, label = "", name, ...rest }: InputProps) => {
+const Input = ({ type, label = "", name, classname, ...rest }: InputProps) => {
   const [inputState, setInputState] = useState(false);
   const [viewPW, setViewPW] = useState(false);
   return (
-    <div className={styles.content}>
+    <div className={`${styles.content} ${classname}`}>
       <label className={styles.label} htmlFor="id">
         {label}
       </label>
@@ -23,13 +24,12 @@ const Input = ({ type, label = "", name, ...rest }: InputProps) => {
         onChange={(e) => setInputState(e.target.value.length > 0)}
         className={`${styles.input} ${inputState ? styles.inputIn : null}`}
         type={viewPW && type === "password" ? "text" : type}
-        id="id"
         name={name}
         {...rest}
       />
-      {type === "password" && (
+      {type === "password" && inputState == true && (
         <div onClick={() => setViewPW(!viewPW)} className={styles.EyeBox}>
-          <img src={viewPW ? "/OpenEye.png" : "/CloseEye.png"}></img>
+          <img src={viewPW ? "/CloseEye.png" : "/OpenEye.png"}></img>
         </div>
       )}
     </div>
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
-
+module.exports = {
+  reactStrictMode: false, // true → false로 변경
+};
 const nextConfig: NextConfig = {
   /* config options here */
 };
