6e2c2b25ecc2232e8dfa50051be1088e76038f15
khkiiim01 <khkiiim01@gmail.com>
2025-02-08 14:21:28 +0900

    added BackHeader

---
+++ app/components/backHeader/BackHeader.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import styles from "./BackHeader.module.css"
+import { useRouter } from "next/navigation";
+
+const BackHeader = () => {
+    const router = useRouter();
+  
+    const handleBackClick = () => {
+      router.back();
+    };
+  
+    return (
+      <div className={styles.navContainer}>
+        <button className={styles.backButton} onClick={handleBackClick}>
+          <img src="/back.svg" alt="뒤로가기" className={styles.icon} />
+        </button>
+      </div>
+    );
+  };
+  
+  export default BackHeader;
+  
