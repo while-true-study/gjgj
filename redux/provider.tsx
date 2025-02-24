3df7a7e7971c620326a20eeeae63692814c75eef
while-true-study <aodaod128@naver.com>
2025-02-24 13:20:01 +0000

    Update build.sh

--- redux/provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client"; // 클라이언트 컴포넌트로 선언
-
-import { Provider } from "react-redux";
-import { store } from "./store";
-
-export default function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
-}
