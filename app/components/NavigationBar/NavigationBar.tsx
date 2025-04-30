3be56062a195b3895f655749293240f02d05964a
while-true-study <aodaod128@naver.com>
2025-04-30 01:45:40 +0000

    [refactor] NavigationBar

--- app/components/NavigationBar/NavigationBar.tsx
+++ app/components/NavigationBar/NavigationBar.tsx
@@ -2,53 +2,49 @@
 import React from "react";
 import styles from "./NavigationBar.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 interface crrstate {
   state: number;
 }
 
+const TAB = {
+  HOME: 1,
+  CATEGORY: 2,
+  MYPAGE: 3,
+} as const;
+
+const tabs = [
+  { id: TAB.HOME, href: "/home.html", name: "home", label: "홈" },
+  {
+    id: TAB.CATEGORY,
+    href: "/category.html",
+    name: "category",
+    label: "카테고리",
+  },
+  { id: TAB.MYPAGE, href: "/mypage.html", name: "mypage", label: "마이페이지" },
+];
+
 const NavigationBar = ({ state }: crrstate) => {
   return (
     <div className={styles.footerBox}>
       <div className={styles.NaviBar}>
-        <div className={styles.NaviBox} onClick={() => console.log("1로 가기")}>
-          <Link href="/home.html">
-            <img
-              src={
-                state === 1
-                  ? "/NavigationBar/selhome.svg"
-                  : "/NavigationBar/home.svg"
-              }
-            ></img>
-          </Link>
-        </div>
-        <div className={styles.NaviBox} onClick={() => console.log("2로 가기")}>
-          <Link href="/category.html">
-            <img
-              src={
-                state === 2
-                  ? "/NavigationBar/selcategory.svg"
-                  : "/NavigationBar/category.svg"
-              }
-            ></img>
-          </Link>
-        </div>
-        <div
-          className={styles.NaviBox}
-          onClick={() => {
-            console.log("3으로 가기");
-          }}
-        >
-          <Link href="/mypage.html">
-            <img
-              src={
-                state === 3
-                  ? "/NavigationBar/selmypage.svg"
-                  : "/NavigationBar/mypage.svg"
-              }
-            ></img>
-          </Link>
-        </div>
+        {tabs.map((tab) => (
+          <div key={tab.id} className={styles.NaviBox}>
+            <Link href={tab.href}>
+              <Image
+                src={
+                  state === tab.id
+                    ? `/NavigationBar/sel${tab.name}.svg`
+                    : `/NavigationBar/${tab.name}.svg`
+                }
+                alt={`${tab.label} 가기`}
+                width={71}
+                height={51}
+              />
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
